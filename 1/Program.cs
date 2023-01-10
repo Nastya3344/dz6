236248df45eb7157fe9ca37145570dd25f326739
nastya <nastik3344@yandex.by>
2023-01-10 23:59:08 +0300

    практ9

---
+++ 1/Program.cs
@@ -0,0 +1,66 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+Console.Clear ();
+
+Console.WriteLine (" Подсчет чисел в строке больше 0");
+
+string tempStr = "";
+int j = 0;
+int count = 0;
+
+// Создаем метод для ввода строки
+
+string InputStr (string message)
+{
+    Console.Write (message);
+    return (Console.ReadLine ());
+}
+
+string str = InputStr ($"Введите числа через запятую: ");
+
+int [] numStr = new int [str.Length];
+
+// Создаем метод перевода строки в числа
+
+int [] ConvertStrToNum (string str)
+{
+    for (int i = 0; i < str.Length; i++)
+    {
+        if (Convert.ToInt32 (str [i]) != 44) // проверка запятой
+        {
+            tempStr += str [i]; // записываем цифры во временную строку пока не встретим запятую
+        }
+        else 
+        {
+            numStr [j] = Convert.ToInt32 (tempStr); // переводим цифры в число
+            j++;
+            tempStr = ""; // обнуляем временную строку
+        }
+        if (i == str.Length - 1) // запоминаем последнее число
+        {
+            numStr [j] = Convert.ToInt32 (tempStr);
+        }
+    }
+    return numStr;
+}
+
+int [] numbers = ConvertStrToNum (str);
+
+
+
+// Console.Write ("Это введенные числа: ");
+
+for (int i = 0; i < numbers.Length; i++)
+{
+//    Console.Write (numbers [i] + " ");
+    if (numbers [i] > 0)
+    {
+        count++;
+    }
+}
+ Console.WriteLine ();
+
+Console.WriteLine ($"Чисел больше нуля: {count}");
---
+++ 2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+// Кординаты точки пересечения по формулам:
+// x = (b2 - b1) / (k1 - k2)
+// y = k1 * (b2 - b1) / (k1 - k2) + b1
+
+Console.WriteLine ("Пересечение прямых y = k1 * x + b1, y = k2 * x + b2");
+
+// Создаем метод для ввода числа пользователем
+
+double InputNum (string message)
+{
+    Console.Write (message);
+    return Convert.ToInt32 (Console.ReadLine ());
+}
+
+double b1 = InputNum ($"Задайте b1: ");
+double k1 = InputNum ($"Задайте k1: ");
+double b2 = InputNum ($"Задайте b2: ");
+double k2 = InputNum ($"Задайте k2: ");
+
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * (b2 - b1) / (k1 - k2) + b1;
+
+Console.WriteLine ($"Точка пересечения прямых: ({x}, {y})");
